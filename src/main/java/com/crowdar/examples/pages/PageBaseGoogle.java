a9cccafa077147005137bd91fbd8482a341dcc91
Guillermo Aguirre <gaguirre@tradehelm.com>
2020-08-06 18:49:00 -0300

    test

--- src/main/java/com/crowdar/examples/pages/PageBaseGoogle.java
+++ src/main/java/com/crowdar/examples/pages/PageBaseGoogle.java
@@ -1,14 +1,15 @@
 package com.crowdar.examples.pages;
 
-import com.crowdar.bdd.cukes.SharedDriver;
-import com.crowdar.core.CucumberPageBase;
+import com.crowdar.core.pageObjects.PageBaseMobile;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
-public class PageBaseGoogle extends CucumberPageBase {
+public class PageBaseGoogle extends PageBaseMobile {
 
-    public PageBaseGoogle(SharedDriver driver){
-        super( driver);
+    public PageBaseGoogle(RemoteWebDriver driver){
+        super(driver);
         BASE_URL = "http://www.google.com.ar";
     }
+
 
 
 }
